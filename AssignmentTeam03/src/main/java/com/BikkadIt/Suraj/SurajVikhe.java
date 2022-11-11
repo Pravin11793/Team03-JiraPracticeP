e30aa49792c87f1e0b9e3d934b59053cf0437c78
SurajSVikhe <vikhesuraj52@gmail.com>
2022-11-11 17:55:33 +0530

    TEAM-03:create your name packege Surajvikhe

---
+++ AssignmentTeam03/src/main/java/com/BikkadIt/Suraj/SurajVikhe.java
@@ -0,0 +1,10 @@
+package com.BikkadIt.Suraj;
+
+public class SurajVikhe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to my world");
+	}
+
+}
