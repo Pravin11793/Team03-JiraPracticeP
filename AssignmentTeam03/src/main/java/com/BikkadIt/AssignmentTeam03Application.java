22a93aaf64fb84e4ded5ed04746707d45c429ab9
pravin11793 <pravinpawar11793@gmail.com>
2022-11-11 17:15:21 +0530

    TEAM Sprint 1: project created

---
+++ AssignmentTeam03/src/main/java/com/BikkadIt/AssignmentTeam03Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentTeam03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentTeam03Application.class, args);
+	}
+
+}
